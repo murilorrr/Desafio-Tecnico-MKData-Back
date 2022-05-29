6180f4f364ef5455b5f58f5eec63125ad14b8e7a
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-29 19:12:57 -0300

    feat: adiciona controller advice para tratar o erro de validação

--- src/main/java/com/mkdata/app/controllers/ErroController.java
+++ src/main/java/com/mkdata/app/controllers/ErroController.java
@@ -1,12 +1,18 @@
 package com.mkdata.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.mkdata.app.commons.ErroNaoEncontrado;
 import com.mkdata.app.commons.ErrorMessage;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.validation.FieldError;
 
 @ControllerAdvice
 public class ErroController {
@@ -15,5 +21,17 @@ public class ErroController {
     ErrorMessage err = new ErrorMessage(e.getMessage());
     err.setStatus(HttpStatus.NOT_FOUND.value());
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+    return errors;
   }
 }
