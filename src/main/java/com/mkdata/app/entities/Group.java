21d7c439f1e0c627b739482437172077d02cbde1
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-30 00:38:34 -0300

    fix: exception Could not write JSON: Infinite recursion

--- src/main/java/com/mkdata/app/entities/Group.java
+++ src/main/java/com/mkdata/app/entities/Group.java
@@ -1,10 +1,12 @@
 package com.mkdata.app.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -33,7 +35,9 @@ public class Group {
   @NotNull(message = "Activated is mandatory")
   private boolean activated;
 
-  @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+  @OneToMany(mappedBy = "group", cascade = CascadeType.ALL,
+      orphanRemoval = true, fetch = FetchType.LAZY)
+  @JsonIgnore
   private List<Customer> customers;
 
   public boolean getActivated() {
