75acff8e73acd7b6ad450a9471333ed0ffed7eb7
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-30 18:18:40 -0300

    fix: retura pasta back-end pois o spring não inicializava, utilizando agora back e front em pastas separadas

--- src/main/java/com/mkdata/app/controllers/CustomerController.java
+++ src/main/java/com/mkdata/app/controllers/CustomerController.java
@@ -9,6 +9,7 @@ import com.mkdata.app.services.CustomerService;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CustomerController {
   
   @Autowired
